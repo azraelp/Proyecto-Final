cf2d55a4a930cb8ae61d5cfb825c10515643ccc0
Alan <160496328+alan-fernandez1@users.noreply.github.com>
2024-03-05 18:58:29 +0100

    Update info.php

--- Assets/web/info.php
+++ Assets/web/info.php
@@ -1,12 +1,9 @@
 <?php
-// Obtener la URL proporcionada por el usuario
+
 $url = $_GET['url'];
 
-echo "tu url es: " .$url;
-
-// Realizar una solicitud HTTP a la URL proporcionada
+// Realiza una solicitud HTTP
 $response = file_get_contents($url);
 
-// Imprimir la respuesta
 echo $response;
 ?>
