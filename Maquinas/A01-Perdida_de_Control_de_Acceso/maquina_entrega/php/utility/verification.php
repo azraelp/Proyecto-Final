7941f38030cf4bc7739326ef7b57ed8cee69c5c1
Jose REche <jose.reche.7e7@itb.cat>
2024-03-13 12:26:08 +0100

    alternate_entrega
    Entrega de solo web, sin nada adicional.

---
+++ Maquinas/A01-Perdida_de_Control_de_Acceso/maquina_entrega/php/utility/verification.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <title>Pagina de Verificacio</title>
+   </head>
+   <body>
+      <?php
+
+    $USER_FORM = $_POST["sUser"];
+    $PASS_FORM = $_POST["sPassword"];
+    $PASS_VULN = $_POST["sPassword2"];
+
+    if ($USER_FORM == "web-admin") {
+        if ($PASS_FORM == "Admin1234567") {
+        //if (password_verify($PASS_FORM,$row['Pass'])) {
+            echo "user valid";
+            echo "<br />";
+            session_start();
+            $_SESSION["Ussr"] = $_POST["sUser"];
+            echo "Session var is: " . $_SESSION["Ussr"];
+            echo "<br />";
+            //
+            header("Location:../pagines/home.php");
+        } elseif (isset($PASS_VULN)) { 
+            echo "user valid -- recuperado";
+            echo "<br />";
+            session_start();
+            $_SESSION["Ussr"] = $_POST["sUser"];
+            echo "Session var is: " . $_SESSION["Ussr"];
+            echo "<br />";
+            //
+            header("Location:../pagines/home.php");
+        } else {
+
+            echo "Invalid Password";
+            header("Location:../pagines/login.php?err=1");
+            exit();
+        }
+    } else {
+        echo "Invalid User";
+        header("Location:../pagines/login.php?err=2");
+        exit();
+    }
+
+    mysqli_close($conn);
+    
+
+      ?>
+   </body>
+</html>
